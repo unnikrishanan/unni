8adf855ca7f022af9c7ce4f27874aebc6414d0f0
Vishnu KR <vishnukr1101@gmail.com>
2022-07-12 12:34:44 +0530

    Initial commit

---
+++ bin/unni.dart
@@ -0,0 +1,10 @@
+void main(List<String> arguments) {
+  String name="UNNI";
+  int age=23;
+  String email_id="unniedayaranz004";
+  double cgpa=1.4+age;
+  print('My name is ${name}');
+  print('Age: ${age}years old');
+  print('Email: ${email_id}@gmail.com');
+  print('CGPA ===: ${cgpa+cgpa}');
+}
---
+++ test/unni_test.dart
@@ -0,0 +1,8 @@
+import 'package:unni/unni.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
